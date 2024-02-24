328c626473e57611709c522c67f314ffbc343f50
Miloslav Lejcek <lejcek.mila@seznam.cz>
2024-02-24 12:09:20 +0100

    Add some constants with tags name

--- src/main/java/com/trixi/demo/constant/Constant.java
+++ src/main/java/com/trixi/demo/constant/Constant.java
@@ -3,4 +3,13 @@ package com.trixi.demo.constant;
 public class Constant {
 
     public static final String baseURL = "https://www.smartform.cz/download/kopidlno.xml.zip";
+    public static final String villageRootTag = "vf:Obec";
+    public static final String villagePartRootTag = "vf:CastiObci";
+
+    public static final String rootIdAtt = "gml:id";
+
+
+
+    public static final String multiPoint = "gml:MultiPoint";
+    public static final String point = "gml:Point";
 }
