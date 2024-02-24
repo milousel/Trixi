c9ce3d2f28511ac2cb33820b96180b32c035530c
Miloslav Lejcek <lejcek.mila@seznam.cz>
2024-02-24 14:31:17 +0100

    add all tag names into Constant

--- src/main/java/com/trixi/demo/constant/Constant.java
+++ src/main/java/com/trixi/demo/constant/Constant.java
@@ -3,13 +3,31 @@ package com.trixi.demo.constant;
 public class Constant {
 
     public static final String baseURL = "https://www.smartform.cz/download/kopidlno.xml.zip";
+
+    //Tags
     public static final String villageRootTag = "vf:Obec";
     public static final String villagePartRootTag = "vf:CastiObci";
+    public static final String regionTag = "obi:Okres";
+    public static final String regionCodeTag = "oki:Kod";
+    public static final String pouTag = "obi:Pou";
+    public static final String puiTag = "pui:Kod";
+    public static final String villageCodeTag = "oki:Kod";
+    public static final String villageNameTag = "obi:Nazev";
+    public static final String villageStatusTag = "obi:StatusKod";
+    public static final String validFromTag = "obi:PlatiOd";
+    public static final String transactionIdTag = "obi:IdTransakce";
+    public static final String globalChangeProposalIdTag = "obi:GlobalniIdNavrhuZmeny";
+    public static final String nutsLauTag = "obi:NutsLau";
+    public static final String geometryTag = "obi:Geometrie";
+    public static final String definitionPointTag = "obi:DefinicniBod";
+    public static final String multiPointTag = "gml:MultiPoint";
+    public static final String pointTag = "gml:Point";
+    public static final String pointMembersTag = "gml:pointMembers";
+    public static final String posTag = "gml:pos";
+    public static final String linguisticCharacteristicTag = "obi:MluvnickeCharakteristiky";
 
-    public static final String rootIdAtt = "gml:id";
-
-
-
-    public static final String multiPoint = "gml:MultiPoint";
-    public static final String point = "gml:Point";
+    //Attributes
+    public static final String idAtt = "gml:id";
+    public static final String nameAtt = "srsName";
+    public static final String dimensionAtt = "srsDimension";
 }
